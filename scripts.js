858dbde5c748cb1d70fa0927230708a7abb3d8e4
Raphael Wegmueller <github@rofe.com>
2020-02-11 16:18:23 -0800

    make all images webp (#112)

--- scripts.js
+++ scripts.js
@@ -117,7 +117,7 @@
 
   const itemTransformer = (item) => ({
     ...item,
-    hero: `${item.hero}?width=256`,
+    hero: `${item.hero}?width=256&auto=webp`,
     date: new Date(item.date * 1000).toLocaleDateString('en-US', {
       day: '2-digit',
       month: '2-digit',
@@ -295,7 +295,7 @@
 
               const avatarURL = /<img src="(.*?)">/.exec(main)[1];
               const authorDiv = document.createElement('div');
-              authorDiv.innerHTML = '<img class="lazyload" data-src="' + avatarURL + '?width=120"> \
+              authorDiv.innerHTML = '<img class="lazyload" data-src="' + avatarURL + '?width=120&auto=webp"> \
                 <span class="post-author">by <a href="' + pageURL + '">' + author + '</a></span> \
                 <span class="post-date">' + date + '</span> \
                 ';
