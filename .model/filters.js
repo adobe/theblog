d4f633a38e69673e19e414578b3d4640942e1726
Ryan Clayton <rclayton@adobe.com>
2020-06-19 01:11:42 -0600

    Updated based off comments: * Added background overlay when dropdown is open. * Dropdown and overlay adjust to mobile appearance at mobile breakpoint. * Updated dropdown button styles. * Added Apply button to bottom of dropdown at all breakpoints. * Updated JS functionality for filter clearing. * Added static filter list.

--- .model/filters.js
+++ .model/filters.js
@@ -3,51 +3,82 @@
  * Set up a click event to show/hide dropdown menu.
  */
 function handleDropdownButtons () {
-    const getFilterBtns = document.querySelectorAll('.filter-btn');
-
-    getFilterBtns.forEach((filterBtn) => {
-        filterBtn.addEventListener('click', (event) => {
+    const getFilterButtons = document.querySelectorAll('.filter-btn');
+    const body = document.querySelector('body');
+    getFilterButtons.forEach((filterButton) => {
+        filterButton.addEventListener('click', (event) => {
             event.stopPropagation();
             const currentDropdown =  event.currentTarget;
             const dropdownContainer = currentDropdown.parentElement;
+            const categoryTitleName = currentDropdown.textContent;
+            const categoryTitle = dropdownContainer.querySelector('.category h2');
+            const clearCurrentFiltersButton = dropdownContainer.querySelector('.filter-clear')
+            // Populate category title.
+            categoryTitle.textContent = categoryTitleName;
             // toggle dropdown menu open
-            currentDropdown.parentElement.classList.toggle('is-open');
+            dropdownContainer.classList.toggle('is-open');
+            if(!body.classList.contains('page-overlay')) {
+                body.classList.add('page-overlay');
+            }
             // Set up document click event to close dropdowns.
             const documentClick = (event) => {
-                const clickInDropdown = currentDropdown.parentElement.contains(event.target);
-                const isOpen = currentDropdown.parentElement.classList.contains('is-open')
+                const clickInDropdown = dropdownContainer.contains(event.target);
+                const isOpen = dropdownContainer.classList.contains('is-open');
                 if(isOpen && clickInDropdown !== true) {
-                    dropdownContainer.classList.remove('is-open');
+                    closeDropdown(dropdownContainer, body);
                     document.removeEventListener('click', documentClick, false);
+                    clearCurrentFiltersButton.removeEventListener('click', clearCurrentFiltersClick, false);
                 }
             }
+            // Set up clearCurrentFiltersButton click event.
+            const clearCurrentFiltersClick = (event) => {
+                const currentFilterOptions = dropdownContainer.querySelectorAll('input:checked');
+                clearCurrentFilters(currentFilterOptions);
+            }
             document.addEventListener('click', documentClick, false);
+            clearCurrentFiltersButton.addEventListener('click', clearCurrentFiltersClick, false);
         });
     });
 }
 
 /**
- * Clear all selected filters.
+ * Close dropddown
  */
-function clearFilters (allFilterOptions) {
+function closeDropdown (dropdownContainer, body) {
+    dropdownContainer.classList.remove('is-open');
+    body.classList.remove('page-overlay');
+}
+
+/**
+ * Clear ALL selected filters.
+ */
+function clearAllFilters (allFilterOptions) {
     allFilterOptions.forEach((option) => {
         const selectedFilters = option.querySelectorAll('input:checked');
-
         selectedFilters.forEach((filter) => {
             filter.checked = false;
         });
     });
 }
 
+/**
+ * Clear current selected filters.
+ */
+function clearCurrentFilters (currentFilterOptions) {
+    currentFilterOptions.forEach((option) => {
+        option.checked = false;
+    });
+}
+
 function initFilterActions () {
     handleDropdownButtons();
     // Clear all button event.
-    const clearFiltersButton = document.querySelector('.filter-clear');
-    clearFiltersButton.addEventListener('click', (event) => {
+    const clearAllFiltersButton = document.querySelector('.filter-clear-all');
+    clearAllFiltersButton.addEventListener('click', (event) => {
         event.stopPropagation();
         const allFilterOptions = document.querySelectorAll('.dropdown-menu');
 
-        clearFilters(allFilterOptions);
+        clearAllFilters(allFilterOptions);
     });
 }
 
