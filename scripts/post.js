d1b39d8aff954f37042a4b30096ce30f01d33631
Marquise Rosier <mrosier@adobe.com>
2020-12-01 08:56:48 -0800

    feat(src/post.js): add more embeds
    add more embeds

--- scripts/post.js
+++ scripts/post.js
@@ -622,7 +622,7 @@ function decorateEmbeds() {
       const id = usp.get('id');
       embedHTML=`
       <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-      <iframe id="twitter-widget-0" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" class="" style="position: static; visibility: visible; width: 550px; height: 476px; display: block; flex-grow: 1;" title="Twitter Tweet" src="https://platform.twitter.com/embed/index.html?creatorScreenName=Adobe&amp;dnt=true&amp;embedId=twitter-widget-0&amp;frame=false&amp;hideCard=false&amp;hideThread=false&amp;id=${id}&amp;lang=en&amp;origin=https%3A%2F%2Fblog.adobe.com%2Fen%2Fpublish%2F2017%2F08%2F28%2Fcreating-motion-graphics-templates-in-adobe-after-effects.html&amp;siteScreenName=Adobe&amp;theme=light&amp;widgetsVersion=ed20a2b%3A1601588405575&amp;width=550px" data-tweet-id="${id}">
+      <iframe scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" style="position: static; visibility: visible; width: 550px; height: 476px; display: block; flex-grow: 1;" title="Twitter Tweet" src="https://platform.twitter.com/embed/index.html?creatorScreenName=Adobe&amp;dnt=true&amp;frame=false&amp;hideCard=false&amp;hideThread=false&amp;id=${id}&amp;lang=en&amp;origin=https%3A%2F%2Fblog.adobe.com%2Fen%2Fpublish%2F2017%2F08%2F28%2Fcreating-motion-graphics-templates-in-adobe-after-effects.html&amp;siteScreenName=Adobe&amp;theme=light&amp;widgetsVersion=ed20a2b%3A1601588405575&amp;width=550px" data-tweet-id="${id}">
       </iframe>
       </div>`
       type='twitter'
@@ -631,7 +631,7 @@ function decorateEmbeds() {
     if($a.href.startsWith('https://www.instagram.com/')) {
       embedHTML=`
         <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-        <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="${url}" allowtransparency="true" allowfullscreen="true" frameborder="0" height="530" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="background: white; max-width: 658px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: 0px 0px 12px; min-width: 326px; padding: 0px;">
+        <iframe class="instagram-media instagram-media-rendered" src="${url}" allowtransparency="true" allowfullscreen="true" frameborder="0" height="530" scrolling="no" style="background: white; max-width: 658px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: 0px 0px 12px; min-width: 326px; padding: 0px;">
         </iframe>
         </div>`;
       type='instagram';
