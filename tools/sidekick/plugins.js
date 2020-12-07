40ab418500fffcb89f89fab9b170a56b15810927
Raphael Wegmueller <github@rofe.com>
2020-12-07 17:04:06 +0100

    feat(sidekick): publish taxonomy (#528)

--- tools/sidekick/plugins.js
+++ tools/sidekick/plugins.js
@@ -314,4 +314,49 @@
       },
     },
   });
+
+  // PUBLISH TAXONOMY -------------------------------------------------------------
+
+  sk.add({
+    id: 'publish-taxonomy',
+    condition: (sk) => {
+      const { config, location } = sk;
+      return config.innerHost
+        && config.host
+        && sk.isEditor()
+        && location.search.includes('file=_taxonomy.xlsx');
+    },
+    override: true,
+    button: {
+      text: 'Publish',
+      action: async () => {
+        const { config } = sk;
+        sk.showModal('Publishing taxonomy...', true);
+        const url = new URL('https://adobeioruntime.net/api/v1/web/helix/helix-services/content-proxy@v2');
+        url.search = new URLSearchParams([
+          ['report', 'true'],
+          ['owner', config.owner],
+          ['repo', config.repo],
+          ['ref', config.ref || 'main'],
+          ['path', '/'],
+          ['lookup', location.href],
+        ]).toString();
+        const resp = await fetch(url.toString());
+        const json = await resp.json();
+        if (!resp.ok || !json.unfriendlyWebUrl) {
+          sk.notify('Failed to publish taxonomy. Please try again later.', 0);
+          console.log('error', JSON.stringify(await resp.json()));
+        }
+        const path = new URL(json.unfriendlyWebUrl).pathname;
+        const purge = await sendPurge(config, path);
+        if (purge.ok) {
+          await fetch(json.unfriendlyWebUrl, {cache: 'reload', mode: 'no-cors'});
+          sk.notify('Taxonomy published');
+        } else {
+          sk.notify('Failed to publish taxonomy. Please try again later.', 0);
+          console.log('error', JSON.stringify(purge));
+        }
+      },
+    },
+  });
 })();
