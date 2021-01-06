da5d4f789ff115d0201700ce20e25ff650690616
Raphael Wegmueller <github@rofe.com>
2021-01-06 09:32:22 +0100

    Interlinks limits per page and paragraph (#562)

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,11 +1,11 @@
 module.exports = function (config) {
   config.set({
-      basePath: '../.',
+      basePath: '..',
       frameworks: ['mocha', 'chai'],
       plugins: ['karma-chrome-launcher', 'karma-chai', 'karma-mocha', 'karma-mocha-reporter'],
       files: [
-        { pattern: 'scripts/taxonomy.js', type: 'module', included: false },
-        { pattern: 'test/unit/taxonomy.test.js', type: 'module' },
+        { pattern: './scripts/*.js', type: 'module', included: false },
+        { pattern: './test/unit/*.test.js', type: 'module' },
         {
           pattern: 'test/features/**/*.json',
           watched: true,
@@ -15,6 +15,9 @@ module.exports = function (config) {
       ],
       reporters: ['mocha'],
       port: 9876,
+      proxies: {
+        '/scripts': '/base/scripts'
+      },
       colors: true,
       logLevel: config.LOG_INFO,
       browsers: ['ChromeHeadless' /*, 'ChromeDebugging'*/],
