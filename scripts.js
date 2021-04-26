1444963601cb81ee3f9f4cb64007a7dcf8ab3ddc
Alexandre Capt <acapt@adobe.com>
2021-04-26 16:35:15 +0200

    feat(web-vitals): extract more martech stuff

--- scripts.js
+++ scripts.js
@@ -32,36 +32,6 @@ function loadCSS(href) {
   document.head.appendChild(link);
 };
 
-
-function checkDX(tags) {
-  const dxtags=`Experience Cloud, Experience Manager, Magento Commerce, Marketo Engage, Target, Commerce Cloud, Campaign, Audience Manager, Analytics, Advertising Cloud,
-      Travel & Hospitality, Media & Entertainment, Financial Services, Government, Non-profits, Other, Healthcare, High Tech, Retail, Telecom, Manufacturing, Education,
-      B2B, Social, Personalization, Campaign Management, Content Management, Email Marketing, Commerce, Analytics, Advertising, Digital Transformation`;
-  const dx=dxtags.split(',').map(e => e.trim());
-  let found=false;
-  tags.split(',').forEach((p) => {
-    p=p.trim();
-    if (dx.includes(p)) found=true;
-  });
-  return found;
-}
-
-/**
- * Return the correct CMP integration ID based on the domain name
- */
-function getOtDomainId() {
-  const domains = {
-    'adobe.com': '7a5eb705-95ed-4cc4-a11d-0cc5760e93db',
-    'hlx.page': '3a6a37fe-9e07-4aa9-8640-8f358a623271',
-    'project-helix.page': '45a95a10-dff7-4048-a2f3-a235b5ec0492',
-    'helix-demo.xyz': 'ff276bfd-1218-4a19-88d4-392a537b6ce3',
-    'adobeaemcloud.com': '70cd62b6-0fe3-4e20-8788-ef0435b8cdb1',
-  };
-  const currentDomain = Object.keys(domains).find(domain => window.location.host.indexOf(domain) > -1);
-
-  return `${domains[currentDomain] || domains[Object.keys(domains)[0]]}`;
-};
-
 /**
  * Returns an image URL with optimization parameters
  * @param {string} url The image URL
@@ -155,33 +125,6 @@ window.blog = function() {
   }
   return { context, language, dateLocale, pageType, TYPE, LANG };
 }();
-
-// Adobe config
-window.fedsMapping = {
-  ko: 'kr'
-};
-
-window.fedsConfig = {
-  locale: window.fedsMapping[window.blog.language] || window.blog.language,
-  content: {
-    experience: 'blogs/blog-gnav',
-  },
-  search: {
-    context: 'blogs',
-    passExperienceName: true,
-  },
-  disableSticky: false,
-  privacy: {
-    otDomainId: getOtDomainId(),
-    footerLinkSelector: '[data-feds-action="open-adchoices-modal"]',
-  },
-};
-
-window.adobeid = {
-  client_id: 'theblog-helix',
-  scope: 'AdobeID,openid',
-  locale: window.blog.language,
-};
 
 // Prep images for lazy loading and use adequate sizes
 let imgCount = 0;
