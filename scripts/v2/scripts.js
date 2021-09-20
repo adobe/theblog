18acdc4836a3c9ae0fd1fc6a7d68b1c8fd63cd26
David Nuescheler <david@nuescheler.ch>
2021-09-20 15:00:12 -0600

    chore: fix generation

--- scripts/v2/scripts.js
+++ scripts/v2/scripts.js
@@ -39,7 +39,7 @@ function sampleRUM(checkpoint, data = {}) {
     const { random, weight, id } = window.hlx.rum;
     if (random && (random * weight < 1)) {
       // eslint-disable-next-line object-curly-newline
-      const body = JSON.stringify({ weight, id, referer: window.location.href, generation: 'biz-gen1', checkpoint, ...data });
+      const body = JSON.stringify({ weight, id, referer: window.location.href, generation: 'blog-gen1', checkpoint, ...data });
       const url = `https://rum.hlx3.page/.rum/${weight}`;
       // eslint-disable-next-line no-unused-expressions
       navigator.sendBeacon(url, body); // we should probably use XHR instead of fetch
